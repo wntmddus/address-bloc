354d387b16db2ec8aaabe390eea2ba448a27763f
Seung Yeon Joo <wntmddussla@hotmail.com>
2016-12-20 16:20:48 -0800

    5 Addressbloc: models assignment

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,23 +1,30 @@
-require_relative 'entry'
+require_relative "entry.rb"
 class AddressBook
-  attr_reader : entries
+  attr_reader :entries
 
   def initializes
     @entries = []
   end
 end
 
-def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
+  def add_entry(name, phone_number, email)
+       # #9
+       index = 0
+       @entries.each do |entry|
+       # #10
+         if name < entry.name
+           break
+         end
+         index+= 1
        end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+       # #11
+       @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -16,6 +16,22 @@ require_relative '../models/address_book'
        expect(book.entries.size).to eq(0)
      end
    end
+   describe "#remove_entry" do
+     it "removes an entry using the name, phone_number, and email address" do
+       book = AddressBook.new
+       book.add_entry("Michael Faraday", "010.210.5181", "michael.faraday@faraday.com")
+
+       name = "Ada Lovelace"
+       phone_number = "010.012.1815"
+       email_address = "augusta.king@locelace.com"
+       book.add_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 2
+       book.remove_entry(name, phone_number, email_address)
+       expect(book.entries.size).to eq 1
+       expect(book.entries.first.name).to eq("Michael Faraday")
+
+     end
+   end
    describe "#add_entry" do
        it "adds only one entry to the address book" do
          book = AddressBook.new
